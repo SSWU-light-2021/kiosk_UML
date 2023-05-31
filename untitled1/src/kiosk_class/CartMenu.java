9f0bc108d4a121b7bb2227023391a41eab9e2e82
theunghee02 <20211150@sungshin.ac.kr>
2023-05-31 23:33:24 +0900

    Create CartMenu, Food Classes

--- untitled1/src/kiosk_class/CartMenu.java
+++ untitled1/src/kiosk_class/CartMenu.java
@@ -1,4 +1,43 @@
 package kiosk_class;
 
 public class CartMenu {
+    private int menuQuantity;
+    private String menuName;
+    private int totalPricePerMenu;
+
+    public int getMenuQuantity() {
+        return menuQuantity;
+    }
+
+    public void setMenuQuantity(int menuQuantity) {
+        this.menuQuantity = menuQuantity;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public int getTotalPricePerMenu() {
+        return totalPricePerMenu;
+    }
+
+    public void setTotalPricePerMenu(int totalPricePerMenu) {
+        this.totalPricePerMenu = totalPricePerMenu;
+    }
+
+    public void addCartMenu() {
+
+    }
+
+    public void removeCartMenu() {
+
+    }
+
+    public void getMenu(String menuName, int menuQuantity) {
+
+    }
 }
--- untitled1/src/kiosk_class/FoodMenu.java
+++ untitled1/src/kiosk_class/FoodMenu.java
@@ -1,5 +1,40 @@
 package kiosk_class;
 
 public class FoodMenu {
+    private String category;
+    private String name;
+    private String image;
+    private int price;
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
