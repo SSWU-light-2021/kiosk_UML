b2801293bdc3ffc54ef96509bf3d82328d0130ba
handagyeong <80438961+handagyeong@users.noreply.github.com>
2023-06-02 21:12:57 +0900

    d

--- untitled1/src/kiosk_class/Controller.java
+++ untitled1/src/kiosk_class/Controller.java
@@ -4,9 +4,14 @@ import javax.swing.*;
 
 public class Controller {
     public  void getCustomerInput(JButton Btn) {
+        if (Btn.getText()=="pickBtn"){
 
+        }
     }
-
+    public String[] ACKorNot(String name, int num){
+        String result[]={name, Integer.toString(num)};
+        return result;
+    }
     public void accept(JButton Btn) {
 
     }
